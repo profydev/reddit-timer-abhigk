66b23b6b4734f7d04702602d54b6c2d9172c3698
Abhi <abhishekgupta0595@gmail.com>
2021-08-15 17:07:52 +1000

    Reset default browser styles (normalize.css)

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
+import normalize from 'normalize.css';
 
 const GlobalStyles = createGlobalStyle`
+${normalize}
     *{
     font-family: 'Bitter', serif;
     font-family: 'Montserrat', sans-serif; 
